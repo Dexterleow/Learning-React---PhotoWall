b3eb5a46889705ab02acd8b0b644aed377a21d9b
dexterleow <gdleow@gmail.com>
2019-02-19 23:34:39 +0800

    add comments in photo

---
+++ src/data/posts.js
@@ -0,0 +1,16 @@
+
+const posts = [{
+    id: 0,
+    description: "beautiful landscape",
+    imageLink: "http://mario.nintendo.com/assets/img/home/char-mario.png"
+}, {
+    id: 1,
+    description: "Aliens???",
+    imageLink: "https://target.scene7.com/is/image/Target/GUEST_3e8ef10c-f1a2-4a47-acb4-ff16ff268345?wid=488&hei=488&fmt=pjpeg"
+}, {
+    id: 2,
+    description: "On a vacation!",
+    imageLink: "https://cdn.arstechnica.net/wp-content/uploads/2018/02/FalconHeavy1-800x591.jpg"
+}]
+
+export default posts;
